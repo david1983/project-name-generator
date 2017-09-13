971a2b34612bb23187ece10639c8a5cbf04169b2
D4v1d3 <y37arf68>
2017-09-13 14:32:32 +0100

    added description

--- frontend/src/state/appstate.js
+++ frontend/src/state/appstate.js
@@ -1,7 +1,7 @@
 import {observable} from "mobx"
 
 class AppState {
-   @observable name = '';
+   @observable name = 'Just press the button below to generate a random name';
 }
 
 export default new AppState()
